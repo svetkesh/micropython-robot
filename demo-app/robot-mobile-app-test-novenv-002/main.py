9d532c71fab8500a50b7351b00796bf54fb22b2a
svetkesh <svetkesh@hotmail.com>
2018-01-14 00:26:30 +0200

    testing raw socket connection

--- demo-app/robot-mobile-app-test-novenv-002/main.py
+++ demo-app/robot-mobile-app-test-novenv-002/main.py
@@ -1,79 +1,39 @@
-from kivy.app import App
-from kivy.garden.joystick import Joystick
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-import time
-# import requests
+'''
+testing raw socket connection
+'''
 
 import socket
+from time import sleep
 
-# sock = socket.socket()
-# sock.connect(('localhost', 9090))
-# sock.send('hello, world!')
-#
-# data = sock.recv(1024)
-# sock.close()
+robot_host = '192.168.101.104'  # hardcodedrobot ip t4m net
+robot_port = 80
 
-# print data
 
-# robot address
-# robot_host = '192.168.101.101'
-# robot_host = '192.168.88.186'  # hardcodedrobot ip t4m net
-robot_host = '192.168.4.1'  # hardcodedrobot ip t4m net
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
 
-class DemoApp(App):
-    def build(self):
-        self.root = BoxLayout()
-        self.root.padding = 50
-        joystick = Joystick()
-        joystick.bind(pad=self.update_coordinates)
-        self.root.add_widget(joystick)
-        self.label = Label()
-        self.root.add_widget(self.label)
-    def update_coordinates(self, joystick, pad):
-        send_status = 'try...'
-        x = str(pad[0])[0:5]
-        y = str(pad[1])[0:5]
-        radians = str(joystick.radians)[0:5]
-        magnitude = str(joystick.magnitude)[0:5]
-        angle = str(joystick.angle)[0:5]
-        # text = "x: {}\ny: {}\nradians: {}\nmagnitude: {}\nangle: {}\nsend data status: {}"
-        # self.label.text = text.format(x, y, radians, magnitude, angle, send_status)
 
-        time.sleep(0.4)
 
-        posx = (float(x)+1)/2
-        print('posx: {}'.format(posx))
-        #
-        # http://192.168.88.186/?headx=0.37
-        # http://192.168.4.1/?headx=0.37
-        #
-        # url = 'http://192.168.88.186/'
-        # payload = {'headx',posx}
-        # r = requests.post(url, data=payload)
-        # print(r.text)
+    posx = 0.1
 
-        # socket implementation in this def
+    while posx < 0.99:
         try:
+            client_socket = socket.socket()  # instantiate
+            client_socket.connect((robot_host, robot_port))  # connect to the server
+            message = 'http://192.168.4.1/?headx=' + str(posx)  # take input
+            client_socket.send(message.encode())  # send message
 
-            sock = socket.socket()
-            send_status = 'sock-OK '
-            sock.connect((robot_host, 80))
-            send_status = 'connect-OK '
-            soc_string = 'http://192.168.4.1/?headx=' + str(posx)
-            send_status = 'string-OK '
-            sock.send(soc_string)
-            send_status = 'send-OK '
-            sock.close()
-            send_status = 'close-OK '
-            send_status = 'sent ok'
+            client_socket.close()  # close the connection
+            sleep(3)
+            #
+            print('posx {} sent'.format(message))
         except:
-            send_status += 'error'
+            print('posx not sent {}'.format(posx))
 
-        # display info
-        text = "x: {}\ny: {}\nradians: {}\nmagnitude: {}\nangle: {}\nsend data status: {}"
-        self.label.text = text.format(x, y, radians, magnitude, angle, send_status)
+        posx += 0.1
 
 
 
-DemoApp().run()
+if __name__ == '__main__':
+    client_program()
