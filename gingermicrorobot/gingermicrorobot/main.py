36ab1c0770aaad9b6e9f987855e4a963c41779b3
svetkesh <svetkesh@hotmail.com>
2018-02-20 17:21:56 +0200

    add ginger

---
+++ gingermicrorobot/gingermicrorobot/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+ver 0.7.9.95 robot modelled in objects. Introducing him here.
+"""
+
+import logging
+
+from gingermicrorobot import GingerMicroRobot as Ginger
+from dcdrive import DCDrive as DCDrive
+from servomotor import ServoMotor as Servo
+
+
+def main():
+    motor = DCDrive(
+        name='motor',
+        letter='A'
+    )
+    servo_dir
+
+    ginger=Ginger(
+
+    )
+
+if __name__ == "__main__":
+    print(
+        'Here I am'
+    )
+
+    main()
+
+
+
