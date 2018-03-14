734c42c42af6bee9d6a62a47648361762db6d3ac
svetkesh <svetkesh@hotmail.com>
2018-03-14 17:34:21 +0200

    add async

--- gingermicrorobot/testing/async01/robotsettings.py
+++ gingermicrorobot/testing/async01/robotsettings.py
@@ -55,7 +55,7 @@ DBG: writing file
 somefile.txt contens>>
 {"wifipassword": "somePassword", "trick": "Mind Blow", "ssid": "some SSID"}
 """
-
+import asyncio
 
 class RobotSettings:
     """
@@ -143,3 +143,33 @@ class RobotSettings:
 # print(new_dict)
 #
 # rs.settings = new_dict
+
+# async test ----
+ssid = "some SSID"
+wifipassword = "somePassword"
+trick = "demo"
+
+c = {
+    'ssid': ssid,
+    'wifipassword': wifipassword,
+    'trick': trick
+}
+
+some_string = 'no tricks here'
+
+rs = RobotSettings(
+    filename='somefile.txt',
+    settings=c              # settings are not saved into file
+)
+
+rs.settings = c           # settings are SAVED into file
+print(rs)
+print(rs.settings)
+print(rs.settings['trick'])
+
+print(asyncio.iscoroutine(rs))
+
+# http://stackabuse.com/python-async-await-tutorial/
+# https://github.com/peterhinch/micropython-async/blob/master/TUTORIAL.md
+
+
