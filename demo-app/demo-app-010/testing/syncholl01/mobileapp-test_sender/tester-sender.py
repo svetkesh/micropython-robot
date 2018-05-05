68c367476fe6010bcfda0af0e51c65e595d86e1a
svetkesh <svetkesh@hotmail.com>
2018-05-05 14:38:32 +0300

    add firmware uasyncio without logging uhttpserver ok

--- demo-app/demo-app-010/testing/syncholl01/mobileapp-test_sender/tester-sender.py
+++ demo-app/demo-app-010/testing/syncholl01/mobileapp-test_sender/tester-sender.py
@@ -187,8 +187,8 @@ def random_sender():
 
     def send_command_data(timeout=0, cycle=0, headx='z', handy='z', turnx='z', runy='z', catch='z'):
         robot_host = '192.168.4.1'  # hardcoded robot ip t4m net
-        robot_port = 80
-        # robot_port = 8080
+        # robot_port = 80
+        robot_port = 8081
         # # print('send_command_data running')
         # self.debug_label.text = 'headx {}\nhandy {}\nturnx {}\nruny {}\ncatch {}'.format(headx,
         #                                                                                  handy,
@@ -262,7 +262,7 @@ def random_sender():
 
         for timeout in range(ini_timeout, 1, -1):
             print('DBG: current timeout: {}, count: {}'.format(timeout / step_timeout, count))
-            for cycle in range(0, 10):
+            for cycle in range(0, 100):
                 # print('DBG: current timeout: {}, cycle: {}'.format(timeout / step_timeout, cycle))
                 generate_commands(timeout, cycle)
                 time.sleep(timeout / step_timeout)
